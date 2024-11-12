ed213a135bdc6f78352b61278d854ed72b77000c
sumayiaakter <aktersumayia92@gmail.com>
2024-11-12 18:50:34 +0600

    Add files via upload

---
+++ length.js
@@ -0,0 +1,49 @@
+// check length
+
+const numbers = [1, 13, 34, 56, 45, 67,69, 56, 90, 89,];
+ console.log(numbers.length);
+
+// check length
+
+// check element
+console.log(numbers[6]);
+const x = [6];
+// check element
+
+indexof
+console.log(numbers.indexOf(45));
+indexof
+
+push
+numbers.push(89)
+console.log(numebrs);
+push
+
+push
+const brands = ["Gucci", "Chanel", "Dior", "Louis Vuitton","Prada" ];
+brands.push("Hermès")
+console.log(brands);
+push
+
+pop
+brands.pop("Hermès")
+console.log(brands);
+pop
+
+unshift
+const cat = ["Gucci", "Chanel", "Dior", "Louis Vuitton","Prada" ];
+brand.unshift("Hermès");
+console.log(cat);
+unshift
+
+shift
+const brand = ["Gucci", "Chanel", "Dior", "Louis Vuitton","Prada" ];
+brand.shift("Hermès"); 
+console.log(brand);
+shift
+
+const member = ["hime","manha","tajmin","nusayba","mim" ];
+const y = member.includes("nusayba");
+console.log(y)
+console.log(Array.isArray(member))
+
